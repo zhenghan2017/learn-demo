c559abe28949db5b5367d1fe285121aa53f405fd
zhenghan2017 <zhenghan2017@163.com>
2019-03-28 19:36:54 +0800

    feat：增加简单的流程图和前端代码的修正

--- react-socket/react/src/App.js
+++ react-socket/react/src/App.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SocketClient from 'socket.io-client';
-let socketClient = null;
+let socketClient;
 
 class App extends Component {
 
   componentDidMount() {
-    // 暂未找到如何优雅的关闭socket连接，这里暂时采取直接将socket对象置为null的方式
+    // 开启socket连接
     socketClient = SocketClient('http://192.168.2.119:2018/news');
+    // 监听发送消息
     socketClient.emit('server', 'hello server');
     socketClient.on('client', data => console.log(data));
     socketClient.on('error', err => console.log(err));
   }
 
   componentWillUnmount() {
-    // 只是关闭了连接，实际使用中，不重置socket对象的话再进入该页面会有两个socket连接产生
+    // 关闭socket连接
     socketClient.close();
   }
 
