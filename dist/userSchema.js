a4d3fa6489b2e24483f25c523e6fb7c24261c3f4
zhenghan2017 <zhenghan2017@163.com>
2020-09-16 13:55:18 +0800

    fix

--- dist/userSchema.js
+++ dist/userSchema.js
@@ -12,6 +12,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.userSchema = exports.getUsers = exports.getUserById = void 0;
 const graphql_1 = require("graphql");
 const mockData_1 = require("./mockData");
+const typeDefine_1 = require("./typeDefine");
 function getUserById(id) {
     const result = mockData_1.baseData.find(item => item.id.toString() === id);
     return result;
@@ -23,31 +24,11 @@ function getUsers() {
     });
 }
 exports.getUsers = getUsers;
-const userType = new graphql_1.GraphQLObjectType({
-    name: 'User',
-    fields: () => ({
-        id: {
-            type: graphql_1.GraphQLNonNull(graphql_1.GraphQLInt)
-        },
-        name: {
-            type: graphql_1.GraphQLNonNull(graphql_1.GraphQLString)
-        },
-        title: {
-            type: graphql_1.GraphQLNonNull(graphql_1.GraphQLString)
-        },
-        age: {
-            type: graphql_1.GraphQLString
-        },
-        sex: {
-            type: graphql_1.GraphQLNonNull(graphql_1.GraphQLString)
-        }
-    })
-});
 const queryType = new graphql_1.GraphQLObjectType({
     name: 'Query',
     fields: () => ({
         user: {
-            type: userType,
+            type: typeDefine_1.UserType,
             args: {
                 id: {
                     type: graphql_1.GraphQLNonNull(graphql_1.GraphQLString)
@@ -56,12 +37,12 @@ const queryType = new graphql_1.GraphQLObjectType({
             resolve: (_source, { id }) => getUserById(id)
         },
         users: {
-            type: graphql_1.GraphQLList(userType),
+            type: graphql_1.GraphQLList(typeDefine_1.UserType),
             resolve: (_source, {}) => __awaiter(void 0, void 0, void 0, function* () { return yield getUsers(); })
         }
     })
 });
 exports.userSchema = new graphql_1.GraphQLSchema({
     query: queryType,
-    types: [userType],
+    types: [typeDefine_1.UserType],
 });
